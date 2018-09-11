49084a088bcf054365dc542e2c666a8b5db67f22
Salocin.TEN <salocin.ten@gmail.com>
2018-09-11 11:36:48 +0800

    Do nearest facility feature and style the title text nicely instead of using heading.

--- js/scripts.js
+++ js/scripts.js
@@ -29,20 +29,6 @@ var map = new H.Map(
 		}
 	});
 
-
-function updatePosition(event) {
-	var youarehere = new H.map.Icon("images/youarehere.png");
-	var userLocation = {
-		lat: event.coords.latitude,
-		lng: event.coords.longitude
-	};
-	map.setCenter(userLocation);
-	var youareheremark = new H.map.Marker(userLocation, {icon: youarehere});
-	map.addObject(youareheremark);
-}
-
-navigator.geolocation.watchPosition(updatePosition);
-
 var facilityIcon = new H.map.Icon("images/FacilityIcon.png");
 
 var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
@@ -78,10 +64,43 @@ flamelinkApp.content.subscribe('facilities', function(error, facilities) {
 				ui.addBubble(infobox);
 			}, false);
 			addMarkerToGroup(facilityLocations, position, 
-                '<h3 class="uk-light">'+facilities[locationId].facilityName+'</h3>'+
+                '<p class="uk-text-large uk-text-bold">'+facilities[locationId].facilityName+'</h3>'+
                 '<p>'+facilities[locationId].facilityDescription+'</p>'
 				);
 			// facilityLocations.addObject(marker);
 		}
 	}
 });
+
+function updatePosition(event) {
+	var youarehere = new H.map.Icon("images/youarehere.png");
+	//Distance to 3 km radius.
+	var minDist = 3000,
+	nearest_text = '*None*',
+    markerDist,
+    // get all objects added to the map
+    objects = facilityLocations.getObjects(),
+    len = facilityLocations.getObjects().length,
+    i;
+	var userLocation = {
+		lat: event.coords.latitude,
+		lng: event.coords.longitude
+	};
+	map.setCenter(userLocation);
+	var youareheremark = new H.map.Marker(userLocation, {icon: youarehere});
+	map.addObject(youareheremark);
+    // iterate over objects and calculate distance between them
+	for (i = 0; i < len; i += 1) {
+		markerDist = objects[i].getPosition().distance(userLocation);
+		// console.log(markerDist);
+		if (markerDist < minDist) {
+			minDist = markerDist;
+			nearest_text = objects[i].getData();
+		}
+	}
+	UIkit.notification('The nearest recycling facility within 3km is: ' + nearest_text, {
+		pos: 'top-right'
+	});
+}
+
+navigator.geolocation.watchPosition(updatePosition);
