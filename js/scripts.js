6537f7a1e545ac998f6507f8d6dbad152a75dedc
Salocin.TEN <salocin.ten@gmail.com>
2018-09-11 23:48:24 +0800

    Add logo and try the routing thing... did not work still... sigh....

--- js/scripts.js
+++ js/scripts.js
@@ -72,6 +72,66 @@ flamelinkApp.content.subscribe('facilities', function(error, facilities) {
 	}
 });
 
+function addRouteShapeToMap(route){
+	var lineString = new H.geo.LineString(),
+	routeShape = route.shape,
+	polyline;
+
+	routeShape.forEach(function(point) {
+		var parts = point.split(',');
+		lineString.pushLatLngAlt(parts[0], parts[1]);
+	});
+
+	polyline = new H.map.Polyline(lineString, {
+		style: {
+			lineWidth: 4,
+			strokeColor: 'rgba(0, 128, 255, 0.7)'
+		}
+	});
+	map.addObject(polyline);
+	map.setViewBounds(polyline.getBounds(), true);
+}
+
+function addManueversToMap(route){
+	var svgMarkup = '<svg width="18" height="18" xmlns="http://www.w3.org/2000/svg">' +
+	'<circle cx="8" cy="8" r="8" fill="#1b468d" stroke="white" stroke-width="1"  />' +
+	'</svg>',
+	dotIcon = new H.map.Icon(svgMarkup, {anchor: {x:8, y:8}}),
+	group = new  H.map.Group(),
+	i,
+	j;
+
+	// Add a marker for each maneuver
+	for (i = 0;  i < route.leg.length; i += 1) {
+		for (j = 0;  j < route.leg[i].maneuver.length; j += 1) {
+			// Get the next maneuver.
+			maneuver = route.leg[i].maneuver[j];
+			// Add a marker to the maneuvers group
+			var marker =  new H.map.Marker({
+				lat: maneuver.position.latitude,
+				lng: maneuver.position.longitude},
+				{icon: dotIcon});
+			marker.instruction = maneuver.instruction;
+			group.addObject(marker);
+		}
+	}
+
+	// group.addEventListener('tap', function (evt) {
+	// 	map.setCenter(evt.target.getPosition());
+	// 	openBubble(
+	// 		evt.target.getPosition(), evt.target.instruction);
+	// }, false);
+
+	// Add the maneuvers group to the map
+	map.addObject(group);
+}
+
+function facilityRouter(result) {
+	var route = result.response.route[0];
+	addRouteShapeToMap(route);
+	addManueversToMap(route);
+};
+
 function updatePosition(event) {
 	var youarehere = new H.map.Icon("images/youarehere.png");
 	//Distance to 3 km radius.
@@ -89,18 +149,31 @@ function updatePosition(event) {
 	map.setCenter(userLocation);
 	var youareheremark = new H.map.Marker(userLocation, {icon: youarehere});
 	map.addObject(youareheremark);
-    // iterate over objects and calculate distance between them
+	var router = platform.getRoutingService(); //To calculate routes.
+    // Show route to the nearest.
 	for (i = 0; i < len; i += 1) {
 		markerDist = objects[i].getPosition().distance(userLocation);
 		// console.log(markerDist);
 		if (markerDist < minDist) {
 			minDist = markerDist;
 			nearest_text = objects[i].getData();
+			var routeParams = {
+				mode: 'fastest;car',
+				routeattributes : 'waypoints,summary,shape,legs',
+				maneuverattributes: 'direction,action',
+				waypoint0: userLocation.lat+','+userLocation.lng,
+				waypoint1: objects[i].getPosition().lat+','+objects[i].getPosition().lng,
+				representation: 'display'
+			};
+			UIkit.notification(routeParams);
+			router.calculateRoute(routeParams, facilityRouter, function(routeErr) {
+				alert(routeErr.message);
+			});
+			UIkit.notification('The nearest recycling facility within 3km is: ' + nearest_text, {
+				pos: 'top-right'
+			});
 		}
 	}
-	UIkit.notification('The nearest recycling facility within 3km is: ' + nearest_text, {
-		pos: 'top-right'
-	});
 }
 
 navigator.geolocation.watchPosition(updatePosition);
